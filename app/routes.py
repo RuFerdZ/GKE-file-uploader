817c09730c95afb859a45d8d35918c20b8eb60a2
Rusiru Fernando <37113657+RuFerdZ@users.noreply.github.com>
2024-09-19 19:00:42 -0400

    reformated code

--- app/routes.py
+++ app/routes.py
@@ -12,14 +12,17 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 500 * 1000 * 1000  # 500 MB
 app.config['CORS_HEADER'] = 'application/json'
 
+
 def allowedFile(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
 
 @app.route('/upload', methods=['POST'])
 def fileUpload():
