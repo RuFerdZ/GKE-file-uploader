b18eff4791a59204fb37edcf7b08bdfed2509703
Rusiru Fernando <37113657+RuFerdZ@users.noreply.github.com>
2024-09-19 19:54:31 -0400

    updated routes to upload files into gcp

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,8 @@ from app import app
 from flask import Flask, jsonify, request
 from werkzeug.utils import secure_filename
 from flask_cors import CORS
+from google.cloud import storage
+from io import BytesIO
 
 import os
 
@@ -24,17 +26,45 @@ def index():
     return "Hello, World!"
 
 
+# @app.route('/upload-local', methods=['POST'])
+# def fileUpload():
+#     file = request.files.getlist('files')
+#     filename = ""
+#     print(request.files, "....")
+#     for f in file:
+#         print(f.filename)
+#         filename = secure_filename(f.filename)
+#         print(allowedFile(filename))
+#         if allowedFile(filename):
+#             f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#         else:
+#             return jsonify({'message': 'File type not allowed'}), 400
+#     return jsonify({"name": filename, "status": "success"})
+
+BUCKET_NAME = "gke-file-upload"
+
+
+def gcs_upload_image(filename: str):
+    storage_client: storage.Client = storage.Client()
+    bucket: storage.Bucket = storage_client.bucket(BUCKET_NAME)
+    bucket.iam_configuration.uniform_bucket_level_access_enabled = False
+    bucket.patch()
+    blob: storage.Blob = bucket.blob(filename.split("/")[-1])
+    blob.upload_from_filename(filename)
+    blob.make_public()
+    public_url: str = blob.public_url
+    print(f"Image uploaded to {public_url}")
+    os.remove(filename)
+    return public_url
+
+
 @app.route('/upload', methods=['POST'])
-def fileUpload():
-    file = request.files.getlist('files')
-    filename = ""
-    print(request.files, "....")
-    for f in file:
-        print(f.filename)
-        filename = secure_filename(f.filename)
-        print(allowedFile(filename))
-        if allowedFile(filename):
-            f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        else:
-            return jsonify({'message': 'File type not allowed'}), 400
-    return jsonify({"name": filename, "status": "success"})
+def upload_image():
+    file = request.files.get('files')
+    if not file:
+        return jsonify({'error': 'A File is required'}), 400,
+    tmp_file = f'files/{file.filename}'
+    file.save(tmp_file)
+    url = gcs_upload_image(tmp_file)
+    # os.remove(tmp_file)
+    return jsonify({'url': url})
